b75bfc4eb9e7abe9e99ad4df5e85111c0c29cad4
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-09-09 17:16:34 +0300

    Show list function in examples

--- examples/s3.rs
+++ examples/s3.rs
@@ -4,20 +4,24 @@ use geckopanda::prelude::*;
 fn main() -> Result<()> {
     let config_data = include_str!("../s3config.toml");
     let storage = S3Storage::new(config_data)?;
-    println!("created s3 storage");
+    println!("Created S3 storage");
 
     let file_id = storage.create_blocking("example.file")?;
-    println!("created file id {file_id}");
+    println!("Created file ID: {file_id}");
 
     let data = "example file content";
     storage.update_blocking(&file_id, data.as_bytes())?;
-    println!("uploaded data: {data:?}");
+    println!("Uploaded data: {data:?}");
 
     let drive_data = String::from_utf8(storage.get_blocking(&file_id)?)?;
     assert_eq!(data, &drive_data);
-    println!("downloaded data: {drive_data:?}");
+    println!("Downloaded data: {drive_data:?}");
+
+    let total_size: u64 = storage.list_blocking().unwrap().iter()
+        .map(|metadata| metadata.size).sum();
+    println!("Total size: {total_size} bytes");
 
     storage.delete_blocking(&file_id)?;
-    println!("deleted file id {file_id}");
+    println!("Deleted file ID: {file_id}");
     Ok(())
 }

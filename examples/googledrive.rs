a36f790f71b7cb88615d86a7582aff63aea47ba7
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-09-09 17:06:54 +0300

    Rename synchronous functions blocking

--- examples/googledrive.rs
+++ examples/googledrive.rs
@@ -6,18 +6,18 @@ fn main() -> Result<()> {
     let storage = GoogleDriveStorage::new(client_secret, "../token_cache.json")?;
     println!("created google drive storage");
 
-    let file_id = storage.create_sync("example.file")?;
+    let file_id = storage.create_blocking("example.file")?;
     println!("created file id {file_id}");
 
     let data = "example file content";
-    storage.update_sync(&file_id, data.as_bytes())?;
+    storage.update_blocking(&file_id, data.as_bytes())?;
     println!("uploaded data: {data:?}");
 
-    let drive_data = String::from_utf8(storage.get_sync(&file_id)?)?;
+    let drive_data = String::from_utf8(storage.get_blocking(&file_id)?)?;
     assert_eq!(data, &drive_data);
     println!("downloaded data: {drive_data:?}");
 
-    storage.delete_sync(&file_id)?;
+    storage.delete_blocking(&file_id)?;
     println!("deleted file id {file_id}");
     Ok(())
 }
--- examples/localdisk.rs
+++ examples/localdisk.rs
@@ -5,18 +5,18 @@ fn main() -> Result<()> {
     let storage = LocalDiskStorage::new("../storagecache")?;
     println!("created local disk storage");
 
-    let file_id = storage.create_sync("example.file")?;
+    let file_id = storage.create_blocking("example.file")?;
     println!("created file id {file_id}");
 
     let data = "example file content";
-    storage.update_sync(&file_id, data.as_bytes())?;
+    storage.update_blocking(&file_id, data.as_bytes())?;
     println!("uploaded data: {data:?}");
 
-    let drive_data = String::from_utf8(storage.get_sync(&file_id)?)?;
+    let drive_data = String::from_utf8(storage.get_blocking(&file_id)?)?;
     assert_eq!(data, &drive_data);
     println!("downloaded data: {drive_data:?}");
 
-    storage.delete_sync(&file_id)?;
+    storage.delete_blocking(&file_id)?;
     println!("deleted file id {file_id}");
     Ok(())
 }

fdcc8a81fe9124238a2f95463bdb204e55c8275c
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-11-24 11:18:33 +0200

    Refactor tests

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,31 +1,30 @@
-use anyhow::Result;
 use geckopanda::prelude::*;
 use std::fs::read_to_string;
 
 #[test]
 #[cfg(feature = "localdisk")]
-fn full_disk() -> Result<()> {
+fn localdisk() {
     let storage = LocalDiskStorage::new("storagecache").unwrap();
-    test_storage(storage)
+    test_storage(storage);
 }
 
 #[test]
 #[cfg(feature = "google-drive")]
-fn full_google_drive() -> Result<()> {
+fn google_drive() {
     let client_secret = read_to_string("client_secret.json").unwrap();
     let storage = GoogleDriveStorage::new(&client_secret, "token_cache.json").unwrap();
-    test_storage(storage)
+    test_storage(storage);
 }
 
 #[test]
 #[cfg(feature = "amazon-s3")]
-fn full_s3() -> Result<()> {
+fn amazon_s3() {
     let config_data = read_to_string("s3config.toml").unwrap();
     let storage = S3Storage::new(&config_data).unwrap();
-    test_storage(storage)
+    test_storage(storage);
 }
 
-fn test_storage(storage: impl Storage) -> Result<()> {
+fn test_storage(storage: impl Storage) {
     let file_count = storage.list_blocking().unwrap().len();
     let file_id = storage.create_blocking("test.file").unwrap();
     assert_eq!(file_count + 1, storage.list_blocking().unwrap().len());
@@ -36,5 +35,4 @@ fn test_storage(storage: impl Storage) -> Result<()> {
     assert_eq!(file_count + 1, storage.list_blocking().unwrap().len());
     storage.delete_blocking(&file_id).unwrap();
     assert_eq!(file_count, storage.list_blocking().unwrap().len());
-    Ok(())
 }
